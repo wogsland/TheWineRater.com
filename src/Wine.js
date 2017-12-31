6ca4999bc437d9a6959155795a55478b54cae695
Bradley Wogsland <bradley@wogsland.org>
2017-12-31 14:15:59 +0100

    routing and a Wine template start

---
+++ src/Wine.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './Wine.css';
+
+class Wine extends Component {
+  render() {
+    return (
+      <div className="Wine">
+        <h1 className="Wine-name">
+          Sp&auml;tburgunder
+        </h1>
+        <p className="Wine-rating">
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Wine;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom'
 import './index.css';
 import App from './App';
+import Wine from './Wine';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <Route exact path="/" component={App}/>
+      <Route path="/wine" component={Wine}/>
+    </div>
+  </BrowserRouter>
+, document.getElementById('root'));
 registerServiceWorker();
